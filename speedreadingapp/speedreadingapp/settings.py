33a56f924587fbbf1a9e93c560be22aec701d34d
Juno-whut <owenrupnow17@gmail.com>
2024-07-13 15:50:13 -0500

    Adding reactjs as frontend

--- speedreadingapp/speedreadingapp/settings.py
+++ speedreadingapp/speedreadingapp/settings.py
@@ -30,7 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'UserLibrary'
+    'rest_framework',
+    'UserLibrary',
+    'users'
 ]
 
 
