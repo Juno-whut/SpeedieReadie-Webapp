69d488c18bd1bcce44f88c45c23597bc09fdd47e
Juno-whut <owenrupnow17@gmail.com>
2024-07-18 19:46:48 -0500

    just moved some folders around in the main directory changed folder structure from SpeedieReadie->venv, frontend, speedreadingapp-> speedreadingapp to SpeedieReadie->venv, speedreadingapp-> speedreadingapp, frontend

--- speedreadingapp/speedreadingapp/settings.py
+++ speedreadingapp/speedreadingapp/settings.py
@@ -1,25 +1,15 @@
-# settings.py
-
 from decouple import config
 from pathlib import Path
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -50,7 +40,7 @@ ROOT_URLCONF = 'speedreadingapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'frontend'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'frontend', BASE_DIR / 'frontend/src'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,19 +59,12 @@ CORS_ORIGIN_WHITELIST = [
 
 WSGI_APPLICATION = 'speedreadingapp.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# Django Database Configuration
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,9 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -109,27 +89,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'frontend',
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Firebase Admin SDK configuration
 FIREBASE_CERT_PATH = config('FIREBASE_CERT_PATH')
 
-# Initialize Firebase Admin SDK
 cred = credentials.Certificate(FIREBASE_CERT_PATH)
 firebase_admin.initialize_app(cred)
 
-# Initialize Firestore DB
 FIRESTORE_DB = firestore.client()
